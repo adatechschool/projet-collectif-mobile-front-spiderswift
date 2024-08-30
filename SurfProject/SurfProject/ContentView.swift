3d1553e0c89d68e0f3326ddf6c1637adbabababc
Bertrand MARIE <photomb09@gmail.com>
2024-08-30 14:47:27 +0200

    Hello Map

--- SurfProject/SurfProject/ContentView.swift
+++ SurfProject/SurfProject/ContentView.swift
@@ -11,15 +11,15 @@ import CoreLocationUI
 
 struct ContentView: View {
     @State private var cameraPosition: MapCameraPosition = .region(MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 46.496687, longitude: -1.784134),
-        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        center: CLLocationCoordinate2D(latitude: 46.915052, longitude: -2.041419),
+        span: MKCoordinateSpan(latitudeDelta: 1.0, longitudeDelta: 1.0)
     ))
     
     var body: some View {
-        Map {
+        Map(position: $cameraPosition) {
             Marker("Les Sables d'Olonne", coordinate: CLLocationCoordinate2D(latitude: 46.496687, longitude: -1.784134)).tint(.blue)
+            Marker("Saint Jea de Monts", coordinate: CLLocationCoordinate2D(latitude: 46.787306, longitude: -2.071404)).tint(.blue)
         }
-    
     
             ZStack {
                 Rectangle()
