e105a1c152162c30ddb05ef18f9bdaf4dbaebc6b
cindylecat <cindylecat5@gmail.com>
2024-09-02 15:41:39 +0200

    routes des boutons

--- SurfProject/SurfProject/ContentView.swift
+++ SurfProject/SurfProject/ContentView.swift
@@ -10,49 +10,58 @@ import SwiftUI
 struct ContentView : View {
     
     func autourDeMoi() {
-          print("Autour de moi")
-      }
-      
+        print("Autour de moi")
+    }
+    
     func lieux() {
-          print("Saint Brévin les Pins")
-      }
-      
+        print("Saint Brévin les Pins")
+    }
+    
     func menu() {
-          print("Aloha")
-      }
+        print("Aloha")
+    }
     
     var body: some View {
-        
-        VStack {
-            ZStack(alignment: .topLeading) {
-                Image("spiderSurfing2")
-                    .resizable(resizingMode: .tile)
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width:400)
-                
-                VStack(alignment: .leading) {
-                    Text("Spider Surf")
-                        .font(.title)
-                        .bold()
-                        .foregroundColor(Color.red)
-                        .multilineTextAlignment(.leading)
-                    Spacer() // Ajoute de l'espace pour pousser le texte en haut
+        NavigationStack {
+            
+            VStack {
+                ZStack(alignment: .topLeading) {
+                    Image("spiderSurfing2")
+                        .resizable(resizingMode: .tile)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width:400)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Spider Surf")
+                            .font(.title)
+                            .bold()
+                            .foregroundColor(Color.red)
+                            .multilineTextAlignment(.leading)
+                        Spacer() // Ajoute de l'espace pour pousser le texte en haut
+                    }
+                    .padding() // Optionnel : ajouter de l'espace autour du texte
                 }
-                .padding() // Optionnel : ajouter de l'espace autour du texte
+                
             }
             
-        }
-        
-        HStack {
-           
-            VStack(spacing:10) {
-                
-                Button(action: {autourDeMoi()}){
-                   
-                    Image(systemName: "compass.drawing")
-                        .foregroundColor(Color.yellow)
-                    Text("Autour de moi")
-                        .foregroundColor(Color.black)
+            
+            
+            //Creation buttons
+            
+            HStack {
+                VStack(spacing:4) {
+                    
+                    Button(action: {autourDeMoi()}){
+                        
+                        Image(systemName: "compass.drawing")
+                            .foregroundColor(Color.yellow)
+                        
+                        NavigationLink("Autour de moi") {
+                            
+                            ListingView()
+                        }
+                        .foregroundColor(.white)
+                    }
                 }
                 
                 .padding()
@@ -60,45 +69,57 @@ struct ContentView : View {
                 .cornerRadius(60.0)
                 .shadow(color: .gray, radius: 20.0, x: 20.0, y: 10.0)
                 
-                //.buttonStyle(BorderedProminentButtonStyle())
             }
             
             
-            VStack(spacing:10){
+            VStack(spacing:4){
                 
                 Button(action: {lieux()}){
                     Image(systemName: "figure.surfing")
                         .foregroundColor(Color.yellow)
-                    Text("Autour d'un lieu")
-                        .foregroundColor(Color.black)
+                    
+                    NavigationLink("Autour d'un lieu") {
+                        DetailView()
+                    }
+                    .foregroundColor(.white)
                 }
                 .padding()
                 .background(LinearGradient(gradient: Gradient(colors:[Color.red,Color.blue]), startPoint: .leading, endPoint: .trailing))
                 .cornerRadius(60.0)
                 .shadow(color: .gray, radius: 20.0, x: 20.0, y: 10.0)
-                //.buttonStyle(BorderedProminentButtonStyle())
             }
             .padding()
-            VStack(spacing:10){
+            
+            VStack(spacing:4){
                 
                 Button(action: {menu()}){
-                    
                     Image(systemName: "list.star")
                         .foregroundColor(Color.yellow)
-                        
-                    Text("Menu")
-                        .foregroundColor(Color.black)
+                    
+                    NavigationLink("Menu") {
+                        DetailView()
+                    }
+                    .foregroundColor(.white)
                 }
                 .padding()
                 .background(LinearGradient(gradient: Gradient(colors:[Color.red,Color.blue]), startPoint: .leading, endPoint: .trailing))
                 .cornerRadius(60.0)
                 .shadow(color: .gray, radius: 20.0, x: 20.0, y: 10.0)
-                //.buttonStyle(BorderedProminentButtonStyle())
             }
         }
     }
 }
 
+    struct DetailView: View {
+        var body: some View {
+            Text("Detail View")
+                .font(.largeTitle)
+                .navigationTitle("Detail")
+        }
+    }
+
+    
 #Preview {
     ContentView()
+    
 }
