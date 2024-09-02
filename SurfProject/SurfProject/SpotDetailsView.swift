118933d4719c5be379c3ff4bf3c108e5b0949722
ModusGit <157380395+ModusGit@users.noreply.github.com>
2024-09-02 11:37:18 +0200

    SpotDetailsView V2

--- SurfProject/SurfProject/SpotDetailsView.swift
+++ SurfProject/SurfProject/SpotDetailsView.swift
@@ -9,7 +9,64 @@ import SwiftUI
 
 struct SpotDetailsView: View {
     var body: some View {
+<<<<<<< HEAD
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+=======
+        
+    GeometryReader { geometry in
+        VStack {
+            Image("audierne")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: geometry.size.width)
+            
+            HStack{
+                Image(systemName: "mappin.and.ellipse")
+                Text("Nom du Spot : ")
+                Text("Audierne")
+            }
+            .font(.title)
+            .padding()
+            
+            VStack(alignment: .leading){
+                
+                Text("Conditions de Surf aujourd'hui :")
+                    .padding()
+                    .bold()
+                
+                HStack{
+                    Image(systemName: "cloud.sun")
+                    Text("Météo : ")
+                    Text("Ensoleillée  |  28°C")
+                }
+                    .padding()
+                
+                HStack{
+                    Image(systemName: "moon.haze")
+                    Text("Marée : ")
+                    Text("Basse 5h03  |  Pleine 12h01")
+                }
+                    .padding()
+                
+                HStack{
+                    Image(systemName: "water.waves")
+                    Text("Houle : ")
+                    Text("2,34 m")
+                }
+                    .padding()
+                
+                HStack{
+                    Image(systemName: "wind")
+                    Text("Vent : ")
+                    Text("Vitesse 15km/h  |  Direction NO")
+                }
+                    .padding()
+            }
+            .padding(.vertical, 15.0)
+            .background(RoundedRectangle(cornerRadius: 10).fill(.beige))
+            }
+        }
+>>>>>>> f891b69 (SpotDetailsView V2)
     }
 }
 
