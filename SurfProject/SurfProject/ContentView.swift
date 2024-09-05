f4a8dc307c92dc64aeac2c7da7fe1c1b9e12e423
ModusGit <157380395+ModusGit@users.noreply.github.com>
2024-09-05 15:18:26 +0200

    version with flynn and maud updated

--- SurfProject/SurfProject/ContentView.swift
+++ SurfProject/SurfProject/ContentView.swift
@@ -9,23 +9,6 @@ import SwiftUI
 
 struct ContentView : View {
     
-<<<<<<< HEAD
-    
-=======
-<<<<<<< HEAD
-    func autourDeMoi() {
-          print("Autour de moi")
-      }
-      
-    func lieux() {
-          print("Saint Brévin les Pins")
-      }
-      
-    func menu() {
-          print("Aloha")
-      }
->>>>>>> 59d6c0f (rebase continue)
-    
     var body: some View {
         
         NavigationStack {
@@ -43,10 +26,6 @@ struct ContentView : View {
                     .padding() // Optionnel : ajouter de l'espace autour du texte
                 }
                 
-                
-                
-                
-                
                 //Creation buttons
                 
                 HStack {
@@ -55,53 +34,10 @@ struct ContentView : View {
                     SupportButton(text: "Menu",icone: "list.star")
                 }
             }
-            
         }
     }
 }
-<<<<<<< HEAD
-    
-=======
 
-=======
-    
-    
-    var body: some View {
-        
-        NavigationStack {
-            
-            VStack {
-                ZStack {
-                    Image("spidermanSurfing")
-                        .resizable(resizingMode: .tile)
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width:400)
-                    
-                    VStack {
-                        AnimationTitle()
-                    }
-                    .padding() // Optionnel : ajouter de l'espace autour du texte
-                }
-                
-                
-                
-                
-                
-                //Creation buttons
-                
-                HStack {
-                    SupportButton(text: "Autour de moi", icone: "compass.drawing")
-                    SupportButton(text: "Autour d'un lieu", icone: "figure.surfing")
-                    SupportButton(text: "Menu",icone: "list.star")
-                }
-            }
-            
-        }
-    }
-}
-    
->>>>>>> f195875 (update de la page principale)
->>>>>>> 59d6c0f (rebase continue)
 #Preview {
     ContentView()
     
