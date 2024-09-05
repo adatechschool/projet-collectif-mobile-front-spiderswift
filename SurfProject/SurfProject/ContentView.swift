16ec7a8e8b0cc5b2f7403c09bb2066853ae0de0c
ModusGit <157380395+ModusGit@users.noreply.github.com>
2024-09-05 14:59:25 +0200

    to mark resolution 6

--- SurfProject/SurfProject/ContentView.swift
+++ SurfProject/SurfProject/ContentView.swift
@@ -9,6 +9,61 @@ import SwiftUI
 
 struct ContentView : View {
     
+<<<<<<< HEAD
+    
+=======
+<<<<<<< HEAD
+    func autourDeMoi() {
+          print("Autour de moi")
+      }
+      
+    func lieux() {
+          print("Saint Brévin les Pins")
+      }
+      
+    func menu() {
+          print("Aloha")
+      }
+>>>>>>> 59d6c0f (rebase continue)
+    
+    var body: some View {
+        
+        NavigationStack {
+            
+            VStack {
+                ZStack {
+                    Image("spidermanSurfing")
+                        .resizable(resizingMode: .tile)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width:400)
+                    
+                    VStack {
+                        AnimationTitle()
+                    }
+                    .padding() // Optionnel : ajouter de l'espace autour du texte
+                }
+                
+                
+                
+                
+                
+                //Creation buttons
+                
+                HStack {
+                    SupportButton(text: "Autour de moi", icone: "compass.drawing")
+                    SupportButton(text: "Autour d'un lieu", icone: "figure.surfing")
+                    SupportButton(text: "Menu",icone: "list.star")
+                }
+            }
+            
+        }
+    }
+}
+<<<<<<< HEAD
+    
+=======
+
+=======
     
     
     var body: some View {
@@ -45,6 +100,8 @@ struct ContentView : View {
     }
 }
     
+>>>>>>> f195875 (update de la page principale)
+>>>>>>> 59d6c0f (rebase continue)
 #Preview {
     ContentView()
     
