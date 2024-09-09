fdf169c71165a60c011b8628d478fff784497c38
Bertrand MARIE <photomb09@gmail.com>
2024-09-09 10:48:20 +0200

    update

---
+++ SurfProject/SurfProject/MapModelView.swift
@@ -0,0 +1,19 @@
+//
+//  MapModelView.swift
+//  SurfProject
+//
+//  Created by Bertrand MARIE on 02/09/2024.
+//
+
+import SwiftUI
+
+struct MapModelView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+
+#Preview {
+    MapModelView()
+}
---
+++ SurfProject/SurfProject/SurfSpotIView_Model.swift
@@ -0,0 +1,18 @@
+//
+//  SurfSpotIView_Model.swift
+//  SurfProject
+//
+//  Created by Bertrand MARIE on 02/09/2024.
+//
+
+import SwiftUI
+
+struct SurfSpotIView_Model: View {
+    var body: some View {
+        var SpotSurfArr:[String] = []
+    }
+}
+
+#Preview {
+    SurfSpotIView_Model()
+}
